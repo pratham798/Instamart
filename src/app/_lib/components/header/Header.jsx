81ab0872edf72f9a4238065b42b250c2bf104d14
pratham798 <prathamjamnal7982@gmail.com>
2024-03-13 15:21:34 +0530

    feat: create typography and header

---
+++ src/app/_lib/components/header/Header.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from 'next/image';
+import BrandLogo from '@/app/_lib/assets/webp/instapayment.webp'
+import styles from './Header.module.css';
+
+export default function Header({HeaderTitle}) {
+  return (
+    <div className={styles.container}>
+      <Image className={styles.logo} src={BrandLogo} alt="brand_logo" />
+      {HeaderTitle}
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,21 @@
 import { Providers } from './_store/provider';
+import Header from './_lib/components/header/Header';
+
+import './styles/typography.css';
 
 export const metadata = {
   title: 'InstaPayments',
   description: 'Developed by Pratham',
+  icons: {
+    icon: './favicon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
+        <Header HeaderTitle="InstaPayments"/>
         <Providers>{children}</Providers>
       </body>
     </html>
